f78cdfeab1688b3343a3d7d222efd85160034fd6
Stanislas Mur <stanislas.mur@gmail.com>
2017-05-29 11:06:07 +0200

    Added the interface for source reader

---
+++ HCEngine/HCEngine/ISourceReader.cs
@@ -0,0 +1,30 @@
+﻿namespace HCEngine
+{
+    /// <summary>
+    /// Interface for reading a source code keyword by keyword.
+    /// </summary>
+    public interface ISourceReader
+    {
+        /// <summary>
+        /// Initializes the reader to a new source. 
+        /// This method serves to avoid intanciating multiple times the reader.
+        /// </summary>
+        /// <param name="source">Source to read</param>
+        void Initialize(string source);
+
+        /// <summary>
+        /// Last keyword read. null when the source is completely read
+        /// </summary>
+        string LastKeyword { get; }
+
+        /// <summary>
+        /// Reads the next keyword, which becomes the <see cref="LastKeyword"/>
+        /// </summary>
+        void ReadNext();
+
+        /// <summary>
+        /// Is the source code completely read?
+        /// </summary>
+        bool ReadingComplete { get; }
+    }
+}
