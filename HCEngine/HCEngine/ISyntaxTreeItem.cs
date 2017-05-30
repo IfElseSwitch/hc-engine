4c5d70a599cf1070d1531057d2e0aca001fca0cf
Stanislas Mur <stanislas.mur@gmail.com>
2017-05-30 14:20:29 +0200

    Added Syntax tree items from specs

--- HCEngine/HCEngine/ISyntaxTreeItem.cs
+++ HCEngine/HCEngine/ISyntaxTreeItem.cs
@@ -6,5 +6,22 @@ namespace HCEngine
 {
     public interface ISyntaxTreeItem
     {
+        /// <summary>
+        /// List of all children nodes
+        /// </summary>
+        List<ISyntaxTreeItem> ChildrenNodes { get; set; }
+
+        /// <summary>
+        /// Logic for setting up the node from a <see cref="ISourceReader" /> .
+        /// </summary>
+        /// <param name="reader">
+        ///   <see cref="ISourceReader" /> for setting up the node.</param>
+        void Setup(ISourceReader reader);
+
+        /// <summary>
+        /// Logic for executing the item, given a scope.
+        /// </summary>
+        /// <param name="scope">Current scope for the execution.</param>
+        void Execute(IExecutionScope scope);
     }
 }
