f929193b7a355f0a6eb0a4d2a448197f1ca217cf
Stanislas Mur <stanislas.mur@gmail.com>
2017-05-31 16:41:41 +0200

    Added tests for Scope factory

---
+++ HCEngine/HCEngine.UnitTesting/ScopeFactoryTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+
+namespace HCEngine.UnitTesting
+{
+    [TestClass]
+    public class ScopeFactoryTest
+    {
+        [TestMethod]
+        public void DefaultScopeFactoryTest()
+        {
+            IScopeFactory factory = new Default.ScopeFactory();
+            IExecutionScope scope = factory.MakeScope();
+            Assert.IsTrue(scope.Contains("firstMethod"));
+            Assert.IsTrue(scope.IsOfType<MethodInfo>("firstMethod"));
+            var method = scope["firstMethod"] as MethodInfo;
+            Assert.IsNotNull(method);
+            Assert.AreEqual("OK", method.Invoke(null, new object[0]));
+            Assert.IsFalse(scope.Contains("secondMethod"));
+        }
+
+        [ExposedCall]
+        public static string FirstMethod()
+        {
+            return "OK";
+        }
+    }
+}
