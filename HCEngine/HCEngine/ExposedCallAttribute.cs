54958a84d38ea9f0c32c5fd52139d35c5512321e
Stanislas Mur <stanislas.mur@gmail.com>
2017-05-31 15:50:25 +0200

    Added Exposed call and type attribute markers

---
+++ HCEngine/HCEngine/ExposedCallAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HCEngine
+{
+    /// <summary>
+    /// Attribute marking a publc static method as accessible from scripts.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class ExposedCallAttribute : Attribute
+    {
+        /// <summary>
+        /// Set if the name of the call should be different in scripts
+        /// </summary>
+        public string NameOverride { get; set; }
+
+        /// <summary>
+        /// Default constructor. ExposedCall has no position parameters.
+        /// </summary>
+        public ExposedCallAttribute() { }
+    }
+}
---
+++ HCEngine/HCEngine/ExposedTypeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCEngine
+{
+    /// <summary>
+    /// Attribute marking a public type as visible from user scripts.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class ExposedTypeAttribute : Attribute
+    {
+        /// <summary>
+        /// Set if the name of the type should be different in scripts
+        /// </summary>
+        public string NameOverride { get; set; }
+
+        /// <summary>
+        /// Set if another type should be used in place of the tagged type
+        /// </summary>
+        public Type LinkToType { get; set; }
+
+        /// <summary>
+        /// Default constructor. ExposedType has no position parameters.
+        /// </summary>
+        public ExposedTypeAttribute() { }
+    }
+}
