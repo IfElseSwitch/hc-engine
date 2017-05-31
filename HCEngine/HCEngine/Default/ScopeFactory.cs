f6a61e91ec50862b1be804b98c5de26c9e52e2bc
Stanislas Mur <stanislas.mur@gmail.com>
2017-05-31 15:43:51 +0200

    Started ScopeFactory implementation

---
+++ HCEngine/HCEngine/Default/ScopeFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace HCEngine.Default
+{
+    class ScopeFactory : IScopeFactory
+    {
+        public IExecutionScope MakeScope()
+        {
+            IExecutionScope scope = new ExecutionScope();
+            //TODO Fill scope with exposed calls and types
+            return scope;
+        }
+    }
+}
