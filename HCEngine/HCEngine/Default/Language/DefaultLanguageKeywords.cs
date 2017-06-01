a01612e40b9d1da4d88037bcb7022e4088608842
Stanislas Mur <stanislas.mur@gmail.com>
2017-06-01 11:50:36 +0200

    Added static class to store language keywords

---
+++ HCEngine/HCEngine/Default/Language/DefaultLanguageKeywords.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCEngine.Default.Language
+{
+    /// <summary>
+    /// Static class containing all of the default language keywords.
+    /// </summary>
+    /// <remarks>Should find better solution to allow diferent keywords for different engine instances.</remarks>
+    public static class DefaultLanguageKeywords
+    {
+        public static string ListBeginSymbol { get; set; } = "(";
+        public static string ListEndSymbol { get; set; } = ")";
+        public static string VariableFirstSymbol { get; set; } = "$";
+        public static string TypingKeyword { get; set; } = "is";
+    }
+}
