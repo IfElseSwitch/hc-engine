83b549501c3ae0b37ca25822c8ea7a1fb309bb88
Stanislas Mur <stanislas.mur@gmail.com>
2017-06-01 11:51:20 +0200

    Implemented Variable and added base classes for syntax structure

---
+++ HCEngine/HCEngine/Default/Language/Statements/AStatement.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCEngine.Default.Language
+{
+    public abstract class AStatement : ISyntaxTreeItem
+    {
+        public abstract List<ISyntaxTreeItem> ChildrenNodes { get; set; }
+
+        public abstract IScriptExecution Execute(IExecutionScope scope);
+
+        public abstract void Setup(ISourceReader reader, IExecutionScope scope);
+    }
+}
---
+++ HCEngine/HCEngine/Default/Language/Statements/Variable.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HCEngine.Default.Language.Statements
+{
+    /// <summary>
+    /// Class representing a variable
+    /// </summary>
+    public class Variable : AOperation
+    {
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Variable()
+        {
+            ChildrenNodes = new List<ISyntaxTreeItem>();
+        }
+
+        /// <summary>
+        /// <see cref="ISyntaxTreeItem.ChildrenNodes"/> 
+        /// </summary>
+        /// <remarks>In this case, the list will always be empty.</remarks>
+        public override List<ISyntaxTreeItem> ChildrenNodes
+        {
+            get; set;
+        }
+
+        /// <summary>
+        /// Identifier of the variable in the scope.
+        /// </summary>
+        public string Identifier { get; private set; }
+
+        /// <summary>
+        /// <see cref="ISyntaxTreeItem.Execute(IExecutionScope)"/> 
+        /// </summary>
+        public override IScriptExecution Execute(IExecutionScope scope)
+        {
+            return new ScriptExecution(Exec(scope));
+        }
+
+        /// <summary>
+        /// <see cref="ISyntaxTreeItem.Setup(ISourceReader, IExecutionScope)"/>
+        /// </summary>
+        public override void Setup(ISourceReader reader, IExecutionScope scope)
+        {
+            if (reader.ReadingComplete)
+                throw new SyntaxException(reader, "Unexpected end of file");
+            string word = reader.LastKeyword;
+            if (!word.StartsWith(DefaultLanguageKeywords.VariableFirstSymbol))
+                throw new SyntaxException(reader, "Variable name does not start with $");
+            Identifier = word;
+            reader.ReadNext();
+        }
+
+        IEnumerator<object> Exec(IExecutionScope scope)
+        {
+            yield return scope[Identifier];
+        }
+    }
+}
