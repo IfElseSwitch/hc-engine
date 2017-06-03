3eb0ffe3ec1d19a212c9b4f96831f1c03d1cb901
Stanislas Mur <stanislas.mur@gmail.com>
2017-06-03 13:16:17 +0200

    Added built in calls

--- HCEngine/HCEngine/Default/Built-in/Calls.cs
+++ HCEngine/HCEngine/Default/Built-in/Calls.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
-namespace HCEngine.Default.Built_in
+namespace HCEngine.Default
 {
     /// <summary>
     /// Static class exposing built-in calls
@@ -19,6 +20,54 @@ namespace HCEngine.Default.Built_in
             for (int i = 0; i < max; ++i)
                 res.Add(i);
             return res;
+        }
+
+        /// <summary>
+        /// Built-in call that checks if a is strictly inferior to b.
+        /// </summary>
+        /// <param name="a">Comparable that should be inferior</param>
+        /// <param name="b">Comparable that should be superior</param>
+        /// <returns>a inferior to b</returns>
+        [ExposedCall]
+        public static bool Inf(IComparable a, IComparable b)
+        {
+            return a.CompareTo(b) < 0;
+        }
+
+        /// <summary>
+        /// Built-in call that checks if a is strictly superior to b.
+        /// </summary>
+        /// <param name="a">Comparable that should be superior</param>
+        /// <param name="b">Comparable that should be inferior</param>
+        /// <returns>a superior to b</returns>
+        [ExposedCall]
+        public static bool Sup(IComparable a, IComparable b)
+        {
+            return a.CompareTo(b) > 0;
+        }
+
+        /// <summary>
+        /// Built-in call that checks if a is equal to b.
+        /// </summary>
+        /// <param name="a">first Comparable</param>
+        /// <param name="b">second Comparable</param>
+        /// <returns>a == b</returns>
+        [ExposedCall]
+        public static bool Eq(IComparable a, IComparable b)
+        {
+            return a.CompareTo(b) == 0;
+        }
+
+        /// <summary>
+        /// Built-in call that adds integers a and b.
+        /// </summary>
+        /// <param name="a">first integer</param>
+        /// <param name="b">Second integer</param>
+        /// <returns>a + b</returns>
+        [ExposedCall]
+        public static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
