0362873b45967e7a01e1b8a2c5ea839e72b360de
Stanislas Mur <stanislas.mur@gmail.com>
2017-06-05 14:29:42 +0200

    Added built-in call for int operations

--- HCEngine/HCEngine/DefaultImplementations/Built-in/Calls.cs
+++ HCEngine/HCEngine/DefaultImplementations/Built-in/Calls.cs
@@ -68,6 +68,18 @@ namespace HCEngine.DefaultImplementations
         }
 
         /// <summary>
+        ///     Built-in call that checks if a is not equal to b.
+        /// </summary>
+        /// <param name="comparable1">first Comparable</param>
+        /// <param name="comparable2">second Comparable</param>
+        /// <returns>a != b</returns>
+        [ExposedCall(NameOverride = "dif")]
+        public static bool Differs(IComparable comparable1, IComparable comparable2)
+        {
+            return !Equals(comparable1, comparable2);
+        }
+
+        /// <summary>
         ///     Built-in call that adds integers a and b.
         /// </summary>
         /// <param name="number1">first integer</param>
@@ -77,6 +89,55 @@ namespace HCEngine.DefaultImplementations
         public static int Add(int number1, int number2)
         {
             return number1 + number2;
+        }
+
+        /// <summary>
+        ///     Built-in call that subtracts integers a and b.
+        /// </summary>
+        /// <param name="number1">first integer</param>
+        /// <param name="number2">Second integer</param>
+        /// <returns>a - b</returns>
+        [ExposedCall(NameOverride = "sub")]
+        public static int Subtract(int number1, int number2)
+        {
+            return number1 - number2;
+        }
+
+        /// <summary>
+        ///     Built-in call that multiplies integers a and b.
+        /// </summary>
+        /// <param name="number1">first integer</param>
+        /// <param name="number2">Second integer</param>
+        /// <returns>a * b</returns>
+        [ExposedCall(NameOverride = "mul")]
+        public static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;
+        }
+
+        /// <summary>
+        ///     Built-in call that divides integers a and b.
+        ///     The result will be the integer part of the division.
+        /// </summary>
+        /// <param name="number1">first integer</param>
+        /// <param name="number2">Second integer</param>
+        /// <returns>a / b</returns>
+        [ExposedCall(NameOverride = "sub")]
+        public static int Divide(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+
+        /// <summary>
+        ///     Built-in call that calculates the remainder of the division of a number by another.
+        /// </summary>
+        /// <param name="number">number to divide</param>
+        /// <param name="modulo">Divider</param>
+        /// <returns>Remainder of number / modulo (number % modulo)</returns>
+        [ExposedCall(NameOverride = "mod")]
+        public static int Modulo(int number, int modulo)
+        {
+            return number % modulo;
         }
     }
 }
