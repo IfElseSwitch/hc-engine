b44c9871030d763abe445f8bfbcbd89b452b2a1b
Stanislas Mur <stanislas.mur@gmail.com>
2017-06-05 11:50:16 +0200

    Added engine tests

---
+++ HCEngine/HCEngine.UnitTesting/EngineTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HCEngine.UnitTesting
+{
+    [TestClass]
+    public class EngineTest
+    {
+        static bool s_check = false;
+
+        [ExposedCall(NameOverride = "testcheck")]
+        public static void TestCheck()
+        {
+            s_check = true;
+        }
+
+        [TestMethod]
+        public void TestEngine()
+        {
+            s_check = false;
+            Engine e = new Engine();
+            s_check = false;
+            e.LoadAndRun("testcheck");
+            Assert.IsTrue(s_check);
+        }
+    }
+}
